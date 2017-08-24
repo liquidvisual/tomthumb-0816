6c543a1e188df091d29f49cfd6424a5b25773d98
Mitchell Warren <hello@liquidvisual.com>
2017-08-24 19:08:41 +1000

    Bootstrap alpha v6 for webvisual (test)

--- src/assets/webvisual/assets/scripts/liquidvisual/equal-height.js
+++ src/assets/webvisual/assets/scripts/liquidvisual/equal-height.js
@@ -1,5 +1,13 @@
 /*
-	EQUAL-HEIGHT.JS - Last updated: 27.10.15
+    EQUAL-HEIGHT.JS - Last updated: 23.11.16
+
+    USAGE: place data-equal-height on each element
+
+    Thanks to CSS Tricks for pointing out this bit of jQuery
+    http://css-tricks.com/equal-height-blocks-in-rows/
+    It's been modified into a function called at page load and then each time the page is resized. One large modification was to remove the set height before each new calculation.
+
+    http://codepen.io/micahgodbolt/pen/FgqLc
 */
 //-----------------------------------------------------------------
 // EQUAL HEIGHT
@@ -12,13 +20,7 @@
     // Variables
     //==================================================
 
-    var equalHeightElement = $('[data-match-height]');
-
-    /* Thanks to CSS Tricks for pointing out this bit of jQuery
-    http://css-tricks.com/equal-height-blocks-in-rows/
-    It's been modified into a function called at page load and then each time the page is resized. One large modification was to remove the set height before each new calculation. */
-
-    // http://codepen.io/micahgodbolt/pen/FgqLc
+    var equalHeightElement = $('[data-equal-height]');
 
     //==================================================
     // Launch
@@ -26,46 +28,46 @@
 
     var equalHeight = function(target){
 
-    	if ($(window).width() >= 768) {
+        if ($(window).width() >= 768) {
 
-    		var currentTallest = 0;
-	    	var currentRowStart = 0;
-	    	var rowDivs = [];
-	    	var $el;
-	    	var topPosition = 0;
+            var currentTallest = 0;
+            var currentRowStart = 0;
+            var rowDivs = [];
+            var $el;
+            var topPosition = 0;
 
-    		$(target).each(function() {
-    			$el = $(this);
-    			$($el).height('auto');
-    			topPosition = $el.position().top;
+            $(target).each(function() {
+                $el = $(this);
+                $($el).height('auto');
+                topPosition = $el.position().top;
 
-    			if (currentRowStart != topPosition) {
-    				for (var currentDiv = 0; currentDiv < rowDivs.length; currentDiv++) {
-    					rowDivs[currentDiv].height(currentTallest);
-    				}
-    				rowDivs.length = 0; // empty the array
-    				currentRowStart = topPosition;
-    				currentTallest = $el.height();
-    				rowDivs.push($el);
-    			} else {
-    				rowDivs.push($el);
-    				currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
-    			}
-    			for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
-    				rowDivs[currentDiv].height(currentTallest);
-    			}
-    		});
-    	} else {
-    		equalHeightElement.height('auto');
-    	}
+                if (currentRowStart != topPosition) {
+                    for (var currentDiv = 0; currentDiv < rowDivs.length; currentDiv++) {
+                        rowDivs[currentDiv].height(currentTallest);
+                    }
+                    rowDivs.length = 0; // empty the array
+                    currentRowStart = topPosition;
+                    currentTallest = $el.height();
+                    rowDivs.push($el);
+                } else {
+                    rowDivs.push($el);
+                    currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+                }
+                for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                    rowDivs[currentDiv].height(currentTallest);
+                }
+            });
+        } else {
+            equalHeightElement.height('auto');
+        }
     };
 
     $(window).on('load', function() {
-    	equalHeight(equalHeightElement);
+        equalHeight(equalHeightElement);
     });
 
     $(window).resize(function(){
-    	equalHeight(equalHeightElement);
+        equalHeight(equalHeightElement);
     });
 
 }(jQuery));
